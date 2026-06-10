23fb22ef6ec7c3bf7e066a546ecae208097ec175
dnexe007 <235278071+dnexe007@users.noreply.github.com>
2026-06-10 18:18:25 +0300

    MassSetup finds components automatically

--- Assets/Car/Scripts/MassSetup.cs
+++ Assets/Car/Scripts/MassSetup.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class MassSetup : MonoBehaviour
 {
-    [SerializeField] private Rigidbody body;
     [SerializeField] private float targetMass;
 
+	[HideInInspector] public Wheel[] wheels;
+	private Rigidbody body;
 
-	public Wheel[] wheels;
 	public float TotalMass { get; private set; }
-
 
 	private void Awake()
 	{
+		body = transform.Find("Body").GetComponent<Rigidbody>();
+		wheels = GetComponentsInChildren<Wheel>();
+
 		float xCount = 10 + wheels.Length;
 
 		float x =  targetMass / xCount;
