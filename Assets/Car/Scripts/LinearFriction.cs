95957a96d38869a8499fe3e89e333142d38a9453
dnexe007 <235278071+dnexe007@users.noreply.github.com>
2026-06-12 17:47:40 +0300

    Architecture improvements
    Common LInearFriction script for all 4 wheels

--- Assets/Car/Scripts/LinearFriction.cs
+++ Assets/Car/Scripts/LinearFriction.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LinearFriction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float sidewaysFrictionCoef = 2;
+	[SerializeField] private float forwardFrictionCoef = 0.1f;
+	[SerializeField] private float fullFrictionSpeed = 0.2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Configuration config;
+
+
+	private void Start()
+	{
+		config = transform.parent.GetComponent<Configuration>();
+	}
+
+
+	private void FixedUpdate()
+	{
+		foreach(Wheel w in config.Wheels) if (w.IsGrounded)
+		{
+			ApplyLinearFriction(w, -w.transform.up, sidewaysFrictionCoef);
+			ApplyLinearFriction(w, w.transform.forward, forwardFrictionCoef);
+		}
+	}
+
+
+	private void ApplyLinearFriction(Wheel wheel, Vector3 direction, float frictionCoef)
+	{
+		direction = Vector3.ProjectOnPlane(direction, wheel.CurrentNormal).normalized;
+		Vector3 slipVel = Vector3.Project(wheel.Rb.velocity, direction);
+
+		float basicFriction = frictionCoef * config.TotalMass;
+		float speedFrictionMult = Mathf.Lerp(0, 1, slipVel.magnitude / fullFrictionSpeed);
+
+		config.RootRigidbody.AddForceAtPosition(
+			basicFriction * speedFrictionMult * -slipVel.normalized,
+			wheel.transform.position
+		);
+	}
 }
--- Assets/Car/Scripts/Wheel.cs
+++ Assets/Car/Scripts/Wheel.cs
@@ -2,27 +2,18 @@ using UnityEngine;
 
 public class Wheel : MonoBehaviour
 {
-	[SerializeField] private float sidewaysFrictionCoef = 2;
-	[SerializeField] private float forwardFrictionCoef = 0.1f;
-	[SerializeField] private float fullFrictionSpeed = 0.1f;
-
-
-	private Configuration config;
 	private readonly AverageNormalData normalData = new();
-
-
 	private static readonly ContactPoint[] contactsBuffer = new ContactPoint[16];
 
 
-	public Rigidbody WheelRigidoby { get; private set; }
+	public Rigidbody Rb { get; private set; }
 	public Vector3 CurrentNormal { get; private set; }
 	public bool IsGrounded { get; private set; }
 
 
 	private void Start()
 	{
-		WheelRigidoby = GetComponent<Rigidbody>();
-		config = transform.parent.GetComponent<Configuration>();
+		Rb = GetComponent<Rigidbody>();
 	}
 
 
@@ -42,27 +33,7 @@ public class Wheel : MonoBehaviour
 	{
 		CurrentNormal = normalData.CalculateAndReset();
 		IsGrounded = CurrentNormal != Vector3.zero;
-		
-		ApplyLinearFriction(-transform.up, sidewaysFrictionCoef);
-		ApplyLinearFriction(transform.forward, forwardFrictionCoef);
 
 		Debug.DrawRay(transform.position, CurrentNormal, Color.red);
-	}
-
-
-	private void ApplyLinearFriction(Vector3 direction, float frictionCoef)
-	{
-		if (!IsGrounded) return;
-
-		direction = Vector3.ProjectOnPlane(direction, CurrentNormal).normalized;
-		Vector3 slipVel = Vector3.Project(WheelRigidoby.velocity, direction);
-
-		float basicFriction = frictionCoef * config.TotalMass;
-		float speedFrictionMult = Mathf.Lerp(0, 1, slipVel.magnitude / fullFrictionSpeed);
-
-		config.RootRigidbody.AddForceAtPosition(
-			basicFriction * speedFrictionMult * -slipVel.normalized,
-			transform.position
-		);
 	}
 }
